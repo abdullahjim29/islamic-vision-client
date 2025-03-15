48df04ef310079e816cb4703405d003557032fcb
abdullahjim29 <abdullahjim2002@gmail.com>
2025-03-16 05:58:16 +0600

    implement 'delete favorite series from favorite list and update and left others favorite as well'

---
+++ src/components/FavoriteCard.jsx
@@ -0,0 +1,57 @@
+import { CiStar } from "react-icons/ci";
+import { IoMdTime } from "react-icons/io";
+import { MdMovie } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const FavoriteCard = ({ favoriteSeries, favorite, setFavorite }) => {
+  const { _id, postar, title, genre, duration, release, ratings, summary } =
+    favoriteSeries;
+
+  const handleDeleteFavorite = (id) => {
+    fetch(`http://localhost:5000/favorite/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const remaingFavorte = [...favorite].filter(fav => fav._id !== id);
+        setFavorite(remaingFavorte);
+      });
+  };
+  return (
+    <div>
+      <div className="card bg-base-100 border border-gray-200 p-4">
+        <figure>
+          <img className="h-[224px] w-full" src={postar} alt={title} />
+        </figure>
+        <div className="flex justify-between items-center my-5">
+          <h2 className="text-xl font-semibold">{title}</h2>
+          <p className="text-[#573AEE] font-bold">{release}</p>
+        </div>
+
+        <div className="flex justify-between items-center">
+          <p className="flex items-center gap-2">
+            <MdMovie className="text-[#573AEE] text-xl" />
+            {genre}
+          </p>
+          <div className="flex items-center gap-4">
+            <p className="flex items-center gap-3">
+              <IoMdTime className="text-[#573AEE] text-xl" />
+              {duration} min
+            </p>
+            <div className="flex items-center gap-2">
+              <CiStar className="text-2xl text-[#573AEE]" />
+              <span>{ratings}</span>
+            </div>
+          </div>
+        </div>
+
+        <button onClick={() => handleDeleteFavorite(_id)} className="font-bold cursor-pointer btn bg-[#573AEE] text-white my-3 w-3/6">
+          Delete Favorite
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FavoriteCard;
--- src/pages/FavoriteSeries.jsx
+++ src/pages/FavoriteSeries.jsx
@@ -1,13 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import SeriesCard from "../components/SeriesCard";
+import FavoriteCard from "../components/FavoriteCard";
+import { useState } from "react";
 
 const FavoriteSeries = () => {
     const loadedFavorite = useLoaderData();
+    const [favorite, setFavorite] = useState(loadedFavorite);
 
     return (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 my-5 gap-y-10 mb-20">
             {
-                loadedFavorite.map(favoriteSeries => <SeriesCard key={favoriteSeries._id} series={favoriteSeries}/>)
+                favorite.map(favoriteSeries => <FavoriteCard key={favoriteSeries._id} favoriteSeries={favoriteSeries} favorite={favorite} setFavorite={setFavorite}/>)
             }
         </div>
     );
